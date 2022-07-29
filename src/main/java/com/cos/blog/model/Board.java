be59e9d661687674e788dec0326b7a35be3bd06d
김현성 <gustjd1209@naver.com>
2022-07-29 16:23:29 +0900

    연관관계 설정

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -30,9 +31,12 @@ public class Board {
     @ColumnDefault("0")
     private int count; // 조회수
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER) // 즉시로딩, 반대 -> 지연로딩, 필요할때 가져오기
     @JoinColumn(name = "userId")
     private User user; // DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다.
+
+    @OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // mappedBy 연관관계의 주인이 아니다 (난 FK가 아니다) DB에 컬럼을 만들지 말아라, 테이블에 생성되는 FK 아니다.
+    private List<Reply> reply;
 
     @CreationTimestamp
     private Timestamp createDate;

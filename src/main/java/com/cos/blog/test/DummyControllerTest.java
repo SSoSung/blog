f94f53ff9bc2c746a4952ad0c7c8cddbf5305772
김현성 <gustjd1209@naver.com>
2022-08-02 17:48:05 +0900

    Jpa, put, delete 및 GlobalExceptionHandler

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -4,15 +4,14 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserReposiroty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -21,6 +20,37 @@ public class DummyControllerTest {
 
     @Autowired
     private UserReposiroty userReposiroty;
+
+    @DeleteMapping("/dummy/user/{id}")
+    public String delete(@PathVariable int id){
+
+        try{
+            userReposiroty.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            return "삭제에 실패햐였습니다. 해당 id는 DB에 없습니다.";
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return "삭제되었습니다. id : " + id;
+    }
+
+    @Transactional
+    @PutMapping("dummy/user/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User requestUser){
+
+        User user = userReposiroty.findById(id).orElseThrow(()->{
+           return new IllegalArgumentException("수정에 실패하였습니다.");
+        });
+
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+
+        //userReposiroty.save(user);
+        // 더티 체킹, 변경감지
+
+        return user;
+    }
 
     @GetMapping("/dummy/users")
     public List<User> list(){
